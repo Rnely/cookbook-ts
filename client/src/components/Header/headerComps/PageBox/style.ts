836ec9e69a3fbff03b91362c6b855500d356e718
Rnely <118372122+Rnely@users.noreply.github.com>
2023-05-19 10:40:01 +0300

    responsive header

--- client/src/components/Header/headerComps/PageBox/style.ts
+++ client/src/components/Header/headerComps/PageBox/style.ts
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { Close, Menu } from '@mui/icons-material';
 import { Button } from '@mui/material';
 import { Box } from '@mui/system';
 
@@ -15,4 +16,30 @@ export const PageButton = styled(Button)`
   &:hover {
     background-color: #2196f3;
   }
+`;
+
+export const ColoredMenu = styled(Menu)`
+  color: white;
+`;
+
+export const RespBox = styled(Box)`
+  width: 100%;
+  height: calc(100% - 76px);
+  color: white;
+  background-color: black;
+  position: fixed;
+  top: 76px;
+  left: 0;
+`;
+
+export const RespMenuBox = styled(Box)`
+  justify-content: center;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+export const ColoredClose = styled(Close)`
+  color: white;
 `;
